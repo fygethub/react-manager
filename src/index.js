c6580140435886fa8e9c6382da8abf6c5e5ba14c
fygethub <13658399878@163.com>
2017-08-03 22:30:52 +0800

    .

--- src/index.js
+++ src/index.js
@@ -33,66 +33,136 @@ import reducer from './reducer';
 import AuthBasic from './components/auth/Basic';
 import RouterEnter from './components/auth/RouterEnter';
 
-const Wysiwyg = (location, cb) => {     // 按需加载富文本配置
+const Wysiwyg = (location, cb) => { // 按需加载富文本配置
     require.ensure([], require => {
         cb(null, require('./components/ui/Wysiwyg').default);
     }, 'Wysiwyg');
 };
 
-const routes =
-    <Route path={'/'} components={Page}>
-        <IndexRedirect to="/app/dashboard/index" />
-        <Route path={'app'} component={App}>
-            <Route path={'form'}>
-                <Route path={'basicForm'} component={BasicForm} />
-            </Route>
-            <Route path={'table'}>
-                <Route path={'basicTable'} component={BasicTable} />
-                <Route path={'advancedTable'} components={AdvancedTable} />
-                <Route path={'asynchronousTable'} components={AsynchronousTable} />
-            </Route>
-            <Route path={'chart'}>
-                <Route path={'echarts'} component={Echarts} />
-                <Route path={'recharts'} component={Recharts} />
-            </Route>
-            <Route path={'ui'}>
-                <Route path={'icons'} component={Icons} />
-                <Route path={'buttons'} component={Buttons} />
-                <Route path={'spins'} component={Spins} />
-                <Route path={'modals'} component={Modals} />
-                <Route path={'notifications'} component={Notifications} />
-                <Route path={'tabs'} component={Tabs} />
-                <Route path={'banners'} component={Banners} />
-                <Route path={'wysiwyg'} getComponent={Wysiwyg} />
-                <Route path={'drags'} component={Drags} />
-                <Route path={'gallery'} component={Gallery} />
-            </Route>
-            <Route path={'animation'}>
-                <Route path={'basicAnimations'} component={BasicAnimations} />
-                <Route path={'exampleAnimations'} component={ExampleAnimations} />
-            </Route>
-            <Route path={'dashboard/index'} component={Dashboard} />
-            <Route path="auth">
-                <Route path="basic" component={AuthBasic} />
-                <Route path="routerEnter" component={RouterEnter} />
-            </Route>
-        </Route>
-        <Route path={'login'} components={Login} />
-        <Route path={'404'} component={NotFound} />
-    </Route>;
+
+const CssModuleT = (location, cb) => {
+    require.ensure([], require => {
+        cb(null, require('./atest').default);
+    }, 'CssModuleT');
+}
+
+
+const routes = <
+    Route path = { '/' }
+components = { Page } >
+    <
+    IndexRedirect to = "/app/dashboard/index" / >
+    <
+    Route path = { 'app' }
+component = { App } >
+    <
+    Route path = { 'form' } >
+    <
+    Route path = { 'basicForm' }
+component = { BasicForm }
+/> <
+/Route> <
+Route path = { 'table' } >
+    <
+    Route path = { 'basicTable' }
+component = { BasicTable }
+/> <
+Route path = { 'advancedTable' }
+components = { AdvancedTable }
+/> <
+Route path = { 'asynchronousTable' }
+components = { AsynchronousTable }
+/> <
+/Route> <
+Route path = { 'chart' } >
+    <
+    Route path = { 'echarts' }
+component = { Echarts }
+/> <
+Route path = { 'recharts' }
+component = { Recharts }
+/> <
+/Route> <
+Route path = { 'ui' } >
+    <
+    Route path = { 'icons' }
+component = { Icons }
+/> <
+Route path = { 'buttons' }
+component = { Buttons }
+/> <
+Route path = { 'spins' }
+component = { Spins }
+/> <
+Route path = { 'modals' }
+component = { Modals }
+/> <
+Route path = { 'notifications' }
+component = { Notifications }
+/> <
+Route path = { 'tabs' }
+component = { Tabs }
+/> <
+Route path = { 'banners' }
+component = { Banners }
+/> <
+Route path = { 'wysiwyg' }
+getComponent = { Wysiwyg }
+/> <
+Route path = { 'drags' }
+component = { Drags }
+/> <
+Route path = { 'gallery' }
+component = { Gallery }
+/> <
+/Route> <
+Route path = { 'animation' } >
+    <
+    Route path = { 'basicAnimations' }
+component = { BasicAnimations }
+/> <
+Route path = { 'exampleAnimations' }
+component = { ExampleAnimations }
+/> <
+/Route> <
+Route path = { 'dashboard/index' }
+component = { Dashboard }
+/> <
+Route path = "auth" >
+    <
+    Route path = "basic"
+component = { AuthBasic }
+/> <
+Route path = "routerEnter"
+component = { RouterEnter }
+/> <
+/Route> <
+Route path = "atest" >
+    <
+    Route path = "cssmodule"
+getComponent = { CssModuleT }
+/> <
+/Route> <
+/Route> <
+Route path = { 'login' }
+components = { Login }
+/> <
+Route path = { '404' }
+component = { NotFound }
+/> <
+/Route>;
 
 // redux 注入操作
 const middleware = [thunk];
 const store = createStore(reducer, applyMiddleware(...middleware));
 console.log(store.getState());
 
-ReactDOM.render(
-    <Provider store={store}>
-        <Router history={hashHistory}>
-            {routes}
-        </Router>
-    </Provider>
- ,
-  document.getElementById('root')
+ReactDOM.render( <
+    Provider store = { store } >
+    <
+    Router history = { hashHistory } > { routes } <
+    /Router> <
+    /Provider>,
+    document.getElementById('root')
 );
 registerServiceWorker();
