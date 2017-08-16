12b8777d78e4362728be5646bd4c9c167b0b914e
fygethub <13658399878@163.com>
2017-08-16 15:13:55 +0800

    🍎句子

--- src/components/ui/draggable/FontEditor/index.jsx
+++ src/components/ui/draggable/FontEditor/index.jsx
@@ -1,67 +1,36 @@
 import React from 'react';
 import pell from 'pell';
+import MediumEditor from 'medium-editor';
 import './fontEditor.less';
 
 export default class FontEditor extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            html: this.props.initText || '输入文字',
-            showEditor: true,
-        };
-        this.handClick = this.handClick.bind(this);
         this.id = this.props.id;
-    }
-
-    handClick() {
-        this.setState({
-            showEditor: true,
-        })
     }
 
     componentDidMount() {
         const _this = this;
         const pellDom = document.getElementById('pell' + this.id);
-        const editor = pellDom && pell.init({
-                element: pellDom,
-                onChange: html => {
-                    this.setState({
-                        html
-                    }, () => this.props.onChange(this.state.html))
-                },
-                styleWithCSS: true,
-                actions: [
-                    'bold',
-                    'underline',
-                    'heading1',
-                    'heading2',
-                    'paragraph',
-                    'italic',
-                    {
-                        name: 'close',
-                        icon: '<b><u><i>X</i></u></b>',
-                        title: 'Custom Action',
-                        result: () => _this.setState({
-                            showEditor: false,
-                        })
-                    }
-                ],
-                classes: {
-                    actionbar: 'pell-actionbar-custom-name',
-                    button: 'pell-button-custom-name',
-                    content: 'pell-content-custom-name'
-                }
-            });
+        var editor = new MediumEditor(pellDom, {
+            toolbar: {
+                buttons: ['bold', 'italic', 'underline', 'strikethrough', 'quote', 'anchor', 'image', 'justifyLeft', 'justifyCenter', 'justifyRight', 'justifyFull', 'superscript', 'subscript', 'orderedlist', 'unorderedlist', 'pre', 'outdent', 'indent', 'h2', 'h3'],
+                static: true,
+                sticky: true
+            }
+        });
+        editor.subscribe('editableInput', function (event, editable) {
+            _this.props.onChange(editable.innerText);
+        });
     }
 
     render() {
-        const show = this.state.showEditor;
         return (<div className="font-editor">
                 <div id={"text-output" + this.id}
-                     onClick={this.handClick}
-                     className="dragText"
-                     dangerouslySetInnerHTML={{__html: this.state.html}}/>
-                <div id={"pell" + this.id} style={{display: show ? 'block' : 'none'}} className="no-cursor"/>
+                     className="dragText"/>
+                <div id={"pell" + this.id} className="no-cursor editable-text">
+                    {this.props.initText}
+                </div>
             </div>
         )
     }
---
+++ src/components/ui/draggable/FontEditor/index_.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import pell from 'pell';
+import './fontEditor.less';
+
+export default class FontEditor extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            html: this.props.initText || '输入文字',
+            showEditor: true,
+        };
+        this.handClick = this.handClick.bind(this);
+        this.id = this.props.id;
+    }
+
+    handClick() {
+        this.setState({
+            showEditor: true,
+        })
+    }
+
+    componentDidMount() {
+        const _this = this;
+        const pellDom = document.getElementById('pell' + this.id);
+        const editor = pellDom && pell.init({
+                element: pellDom,
+                onChange: html => {
+                    this.setState({
+                        html
+                    }, () => this.props.onChange(this.state.html))
+                },
+                styleWithCSS: true,
+                actions: [
+                    'bold',
+                    'underline',
+                    'heading1',
+                    'heading2',
+                    'paragraph',
+                    'italic',
+                    {
+                        name: 'close',
+                        icon: '<b><u><i>X</i></u></b>',
+                        title: 'Custom Action',
+                        result: () => _this.setState({
+                            showEditor: false,
+                        })
+                    }
+                ],
+                classes: {
+                    actionbar: 'pell-actionbar-custom-name',
+                    button: 'pell-button-custom-name',
+                    content: 'pell-content-custom-name'
+                }
+            });
+    }
+
+    render() {
+        const show = this.state.showEditor;
+        return (<div className="font-editor">
+                <div id={"text-output" + this.id}
+                     onClick={this.handClick}
+                     className="dragText"
+                     dangerouslySetInnerHTML={{__html: this.state.html}}/>
+                <div id={"pell" + this.id} style={{display: show ? 'block' : 'none'}} className="no-cursor"/>
+            </div>
+        )
+    }
+}
