0c5b16194bb245947e89a2c4fd4d572b6e0410f8
fygethub <13658399878@163.com>
2017-08-25 15:12:35 +0800

    remove build

--- build/static/js/Admins.658b9fde.chunk.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([4],{741:function(n,c){}});
-//# sourceMappingURL=Admins.658b9fde.chunk.js.map
--- build/static/js/Apps.c9dc9964.chunk.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([3],{742:function(n,c){}});
-//# sourceMappingURL=Apps.c9dc9964.chunk.js.map
